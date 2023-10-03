aa8d7cf074e760a8db4e89c10fe185406213d4d8
quentinmathieu <quentin.mth@gmail.com>
2023-10-03 14:51:52 +0200

    add variations img feature

--- src/Controller/OpenAIController.php
+++ src/Controller/OpenAIController.php
@@ -5,17 +5,36 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
 use App\Service\OpenAiService;
 
 class OpenAIController extends AbstractController
 {
-    #[Route('/openai', name: 'app_open_a_i')]
-    public function index(Request $request, OpenAiService $openAi,): Response
+    #[Route('/imgGen', name: 'img_gen')]
+    public function index( OpenAiService $openAi,): Response
     {
+        $prompt = "Colored photo of a cute young dog (golden retriever) swimmng in the sea, a stick in the , we can see the beach (wich noone on it), and some big rocks on the right side of the photo. There is an halo effect on the photo.";
 
-        $prompt = " Colored middle ages engravinga of cute young cat drinking milk";
+
+          
         $image = $openAi->getImageGenerated($prompt);
+
+
+        return $this->render('open_ai/index.html.twig', [
+            'controller_name' => 'OpenAIController',
+            'image' => $image,
+            'prompt' => $prompt
+        ]);
+    }
+
+    #[Route('/imgVar', name: 'img_var')]
+    public function imgVar( OpenAiService $openAi,): Response
+    {
+        $prompt = null;
+
+        $img = curl_file_create('img/testImgg.png');
+
+        $image = $openAi->getImageVariation($img);  
+        // $image = $openAi->getImageGenerated($prompt);
 
 
         return $this->render('open_ai/index.html.twig', [
--- src/Service/OpenAiService.php
+++ src/Service/OpenAiService.php
@@ -22,12 +22,30 @@ class OpenAiService
         $complete =  $open_ai->image([
             "prompt" => $prompt,
             "n" => 1,
-            "size" => "512x512",
+            "size" => "256x256",
             "response_format" => "url",
          ]);
          $json = json_decode($complete, true);
     
         
         return $json['data'][0]['url'];
+    }
+
+    public function getImageVariation( $imagePath) : string {
+        // dd($imagePath);
+        // echo "<img src='$imagePath' alt=''>";
+        $open_ai_key = $this->param->get('OPENAI_API_KEY');
+
+        $open_ai = new OpenAi($open_ai_key);
+
+        
+        $imgVariation =  $open_ai->createImageVariation([
+            "image" => $imagePath,
+            "n" => 1,
+            "size" => "512x512",
+        ]);
+    
+        
+        return json_decode($imgVariation,true)['data'][0]['url'];
     }
 }
